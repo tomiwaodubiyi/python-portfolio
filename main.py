a0beabb84f01d47628376a88f7c98b3d507fcb93
tomiwaodubiyi <odubiyitomiwa@gmail.com>
2025-05-23 18:17:51 -0700

    Added a description to the columns

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
+#col3 = st.columns(1)
 
 with col1:
     st.image("images/photo.jpeg")
@@ -9,7 +10,9 @@ with col2:
     st.title("Babatomiwa Odubiyi")
     content = """
     Hi I'm Babatomiwa! I'm a Network Security Engineer and Python Programmer. 
-    I built this webapp to showcase my portfolio. You can contact me if you have any inquiries 
-    and I'll be happy to connect with you.
     """
     st.info(content)
+contact_writeup = """ I built this webapp to showcase my portfolio. You can contact me if you have any inquiries 
+    and I'll be happy to connect with you.
+                    """
+st.write(contact_writeup)
