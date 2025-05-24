1f52bd50b3c9561fedc9329b10a64d3dc55e2dd4
tomiwaodubiyi <odubiyitomiwa@gmail.com>
2025-05-23 18:44:41 -0700

    Added images, links to repos, and descriptions

--- main.py
+++ main.py
@@ -17,14 +17,20 @@ contact_writeup = """ I built this webapp to showcase my portfolio. You can cont
                     """
 st.write(contact_writeup)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+ row["image"])
+        st.write(f"[Source code]({row['url']})")
